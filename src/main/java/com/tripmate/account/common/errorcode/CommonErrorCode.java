c1100f194091abaf36ea7c7d14e4cb0914428149
suin <tndls0106@gmail.com>
2024-10-06 19:51:00 +0900

    스웨거 연결

--- src/main/java/com/tripmate/account/common/errorcode/CommonErrorCode.java
+++ src/main/java/com/tripmate/account/common/errorcode/CommonErrorCode.java
@@ -1,15 +1,17 @@
 package com.tripmate.account.common.errorcode;
 
-import com.tripmate.account.common.exception.InvalidErrorException;
+import com.tripmate.account.common.exception.ServerErrorException;
 import lombok.AccessLevel;
-import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 import java.util.HashMap;
 import java.util.Map;
 
-@Getter
+//@Getter
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public enum CommonErrorCode {
+    /*이늄의 각 상수는 이늄클래스가 구현한 인스턴스다.
+      에러메세지는 클라이언트에게 보여지는것으로 자주 수정할 수있으므로 여기서 관리하는게 유지보수에 좋을것이다. */
+
     //0000번 : 성공
     SUCCESS("0000", "성공"),
     // 1000~1099: 클라이언트 유효성 검사 관련 에러 코드
@@ -44,24 +46,39 @@ public enum CommonErrorCode {
     final String code;
     final String message;
 
+    // Getter methods
+    public String getCode(){
+        return code;
+    }
+    public String getMessage(){
+        return message;
+    }
+
     CommonErrorCode(String code, String message) {
         this.code = code;
         this.message = message;
     }
 
+    /*조회할때마다 반복문을 타는 걸로 구현했었는데 빈번하게 호출하는 경우에는 Map에 캐시해두는게 성능측면과 속도면에서 좋아서 수정했다.
+     해시맵은 평균적으로 O(1) 시간 복잡도를 가지고 있기 때문에 빠르게 데이터를 찾을 수 있고
+      반복문을 줄이면 CPU 사용량이 감소하고, 그에 따라 애플리케이션의 전반적인 성능도 개선된다.
+      static 블록으로 반복문은 클래스가 로드될 때 한 번만 실행된다. */
+
     private static final Map<String, CommonErrorCode> errorCodeMap = new HashMap<>();
 
     static {
         for (CommonErrorCode errorCode : CommonErrorCode.values()) {
-            errorCodeMap.put(errorCode.getCode(), errorCode); //0000 ,  SUCCESS("0000", "성공")
+            errorCodeMap.put(errorCode.getCode(), errorCode); //key: 0000 ,  value: SUCCESS("0000", "성공")
         }
     }
 
+    //코드번호로 CommonErrorCode.SUCCESS 인스턴스를 반환
     public static CommonErrorCode fromCode(String code) {
         CommonErrorCode errorCode = errorCodeMap.get(code);
 
         if (errorCode == null) {
-            throw new InvalidErrorException(NO_MATCHING_ERROR_CODE);
+            //TODO 예외이름이 적절하지 못해서 바꿔야 함. 혹시 이렇게 예기치못한 에러가 있으면 새로운 예외를 만들자.
+            throw new ServerErrorException(NO_MATCHING_ERROR_CODE);
         }
         return errorCode;
     }
@@ -69,16 +86,9 @@ public enum CommonErrorCode {
 
 
 /*
+-----클래스가 로드 될떄 실제 동작하는것 !!--------
 public class CommonErrorCode {
     public static final CommonErrorCode SUCCESS = new CommonErrorCode(0000, "성공");
     public static final CommonErrorCode USER_ID_BLANK = new CommonErrorCode(1001, "아이디는 빈 값이 될 수 없습니다.");
-
-    private final int code;
-    private final String message;
-
-    public CommonErrorCode(int code, String message) {
-        this.code = code;
-        this.message = message;
-    }
 }
  */
--- src/main/java/com/tripmate/account/common/reponse/CommonResponse.java
+++ src/main/java/com/tripmate/account/common/reponse/CommonResponse.java
@@ -9,24 +9,24 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class CommonResponse<T> {
 
-    private String code;        // 응답 상태 코드 (예: 0000)
+    private String codeNo;        // 응답 상태 코드 (예: 0000)
     private String message;  // 응답 메시지 (예: "성공")
     private T data;          // 응답에 포함된 데이터, 제네릭 타입 (T)
 
     //이건안씀 응답을 안내려주는경우는 없음 public CommonResponse(){}
     public CommonResponse(CommonErrorCode commonErrorCode, T data) {
-        this.code = commonErrorCode.getCode();
+        this.codeNo = commonErrorCode.getCode();
         this.message = commonErrorCode.getMessage();
         this.data = data;
     }
 
     public CommonResponse(CommonErrorCode commonErrorCode) {
-        this.code = commonErrorCode.getCode();
+        this.codeNo = commonErrorCode.getCode();
         this.message = commonErrorCode.getMessage();
     }
 
     public String getCode(){
-        return code;
+        return codeNo;
     }
 
     public String getMessage(){
