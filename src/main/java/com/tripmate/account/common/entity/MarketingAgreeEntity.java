41e895344c855b73a541880564fe254a5a2cf833
suin <tndls0106@gmail.com>
2024-10-13 18:26:08 +0900

    약관동의

---
+++ src/main/java/com/tripmate/account/common/entity/MarketingAgreeEntity.java
@@ -0,0 +1,4 @@
+package com.tripmate.account.common.entity;
+
+public class MarketingAgreeEntity {
+}
--- src/main/java/com/tripmate/account/common/entity/TemplateEntity.java
+++ src/main/java/com/tripmate/account/common/entity/TemplateEntity.java
@@ -5,27 +5,40 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-//@Entity
-//@Table(name="TEMPLATE_TB")
-//@Getter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//@Builder
-//@FieldDefaults(level = AccessLevel.PRIVATE)
-//@EntityListeners((AuditingEntityListener.class))
-public class TemplateEntity {
-//    @Id
-//    @Column(name="TEMPLATE_SQ")
-//    int templateSq;
-//
-//    @Column(name = "M_CATEGORY")
-//    char mCategory;
-//    char s_category;
-//    String title;
-//    String content;
-//    char use_fl;
-//    reg_user;
-//    reg_dtm;
-//    updt_user;
-//    updt_dtm;
-}
+
+@Entity
+@Table(name="TEMPLATE_TB")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class TemplateEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//MySQL에서 기본 키 필드가 AUTO_INCREMENT로 설정
+    @Column(name="TEMPLATE_SQ",nullable = false)
+    int templateSq;
+    @Column(name = "M_CATEGORY",nullable = false)
+    char mCategory;
+    @Column(name="S_CATEGORY",nullable = false)
+    char sCategory;
+    @Column(name="TITLE",nullable = false)
+    String title;
+    @Column(name = "CONTENT",nullable = false)
+    String content;
+    @Column(name="USE_FL",nullable = false,columnDefinition="CHAR(1) DEFAULT 'Y'")//@PrePersist가 아닌 columnDefinition으로 쓴 이유: JPA뿐만 아니라 데이터베이스에서 직접 쿼리를 실행하여 삽입할 때도 기본값이 적용
+    char useFl='Y';
+//    @Column(name="REG_USER",nullable = false)
+//    String regUser;
+//    @CreatedDate
+//    @Column(name = "REG_DTM",nullable = false,updatable = false)
+//    LocalDateTime regDtm;
+//    @Column(name = "UPDT_USER")
+//    String updtUser;
+//    @LastModifiedDate
+//    @Column(name="UPDT_DTM",nullable = true)
+//    LocalDateTime updtDtm;
+    }
+
